94b85866793db71a8deea45b5af5ef4aff8611b2
UmarWaseem-ui <waseemumree@gmail.com>
2026-06-21 17:03:04 +0500

    Delete Chapter No 2 directory

--- Chapter No 2/Problem 01.py
+++
@@ -1,8 +0,0 @@
-# Sum of two numbers 
-a=3
-b=6
-print(a+b)
-# To find the remainder 
-a=37
-z=5
-print(a%z)
--- Chapter No 2/Problem 02.py
+++
@@ -1,8 +0,0 @@
-# Program to check the Data type 
-a=input("Enter the Value of a :")
-print(type(a))
-
-# Use of Comparison Operator
-a=34
-b=80
-print(a>b)
