22df09dfb2a8ef1aa0289ec3dd8000533d51ced4
UmarWaseem-ui <waseemumree@gmail.com>
2026-06-21 17:08:54 +0500

    Add files via upload

---
+++ Week1/Day2/Chapter No 3/Problem 13.py
@@ -0,0 +1,10 @@
+# Printing of Table
+n=int(input("Enter the Number: "))
+i=1
+for i in range(1, 11):
+    print(f"{n} x {i} = {n*i}")
+
+# sum of 100 Numbers
+i=0
+for i in range(1,101):
+    print(i)
---
+++ Week1/Day2/Chapter No 3/Problem 14.py
@@ -0,0 +1,12 @@
+# Countdown Timer
+i=1
+for i in range(1,11):
+    print(11-i)
+
+# Factorial Program
+n=int(input("Enter the Number: "))
+Product=1
+for i in range(1,n+1):
+    Product= Product * i
+
+print(f"The Factorial of {n} is {Product}")
