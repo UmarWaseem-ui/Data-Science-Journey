a2965af9b5d92f53073a6c34fce0c5c708d94c74
UmarWaseem-ui <waseemumree@gmail.com>
2026-06-21 17:07:06 +0500

    Add files via upload

---
+++ Week1/Day1/Chapter No 2/Problem 04.py
@@ -0,0 +1,17 @@
+# Even or Odd Checker
+num = int(input("Enter number: "))
+
+# Divide the num by 2 and Check the remainder is 0
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Checking if the num is Positive or Negative
+num=int(input("Enter the Number :"))
+
+# Condtion applied 
+if num>0 :
+    print("Positive")
+else:
+    print("Negative")
